38f1bb94447186a630f2c94177962029b4f8a85a
Raj Dave <10864@bpsdoha.edu.qa>
2022-11-05 11:07:45 +0300

    update sql

--- data/database.sql
+++ data/database.sql
@@ -1,32 +1,33 @@
 -- This code creates the database and tables for /data/data.db
 -- There are 5 tables
 
-CREATE TABLE "dm_notify" (
+CREATE TABLE if not exists "dm_notify" (
 	"user_id"	INT NOT NULL,
 	"message"	TEXT DEFAULT 'A new Circular was just posted on the website!'
 );
 
-CREATE TABLE "guild_notify" (
+CREATE TABLE if not exists "guild_notify" (
 	"guild_id"	INTEGER NOT NULL UNIQUE,
 	"channel_id"	INTEGER UNIQUE,
 	"message"	TEXT DEFAULT "There's a new circular up on the website!"
 );
 
-CREATE TABLE "logs" (
+CREATE TABLE if not exists "logs" (
 	"timestamp"	INTEGER NOT NULL,
 	"log_level"	TEXT DEFAULT 'debug',
 	"category"	TEXT,
 	"msg"	TEXT
 );
 
-CREATE TABLE "suggestions" (
+CREATE TABLE if not exists "suggestions" (
 	"user_id"	INTEGER NOT NULL,
 	"message_id"	INTEGER,
 	"message"	TEXT
 );
 
-CREATE TABLE "search_feedback" (
+CREATE TABLE if not exists "search_feedback" (
 	"user_id"	INTEGER,
 	"message_id"	INTEGER,
+	"search_query"	TEXT,
 	"response"	TEXT
 );
