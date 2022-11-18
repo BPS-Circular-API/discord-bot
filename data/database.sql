d120846ca7579461ec1d9f83cb6d1c7a61467f74
Raj Dave <10864@bpsdoha.edu.qa>
2022-11-18 07:41:49 +0300

    add table to sql

--- data/database.sql
+++ data/database.sql
@@ -1,25 +1,25 @@
 -- This code creates the database and tables for /data/data.db
 -- There are 5 tables
 
-CREATE TABLE if not exists "dm_notify" (
+CREATE TABLE IF NOT EXISTS "dm_notify" (
 	"user_id"	INT NOT NULL,
 	"message"	TEXT DEFAULT 'A new Circular was just posted on the website!'
 );
 
-CREATE TABLE if not exists "guild_notify" (
+CREATE TABLE IF NOT EXISTS "guild_notify" (
 	"guild_id"	INTEGER NOT NULL UNIQUE,
 	"channel_id"	INTEGER UNIQUE,
 	"message"	TEXT DEFAULT "There's a new circular up on the website!"
 );
 
-CREATE TABLE if not exists "logs" (
+CREATE TABLE IF NOT EXISTS "logs" (
 	"timestamp"	INTEGER NOT NULL,
 	"log_level"	TEXT DEFAULT 'debug',
 	"category"	TEXT,
 	"msg"	TEXT
 );
 
-CREATE TABLE if not exists "search_feedback" (
+CREATE TABLE IF NOT EXISTS "search_feedback" (
 	"user_id"	INTEGER,
 	"message_id"	INTEGER,
 	"search_query"	TEXT,
@@ -27,4 +27,7 @@ CREATE TABLE if not exists "search_feedback" (
 );
 
 
---CREATE TABLE if not exists "cache" TODO: this sql
+CREATE TABLE IF NOT EXISTS "notif_msgs" (
+	"circular_id"	INTEGER,
+	"msg_id"	INTEGER UNIQUE
+);
